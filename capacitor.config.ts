c187d0fafea08ac334b360879bf815bb41dc32e2
Jonathan Alvarado <jalvarado@sold.com>
2024-09-08 20:55:44 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'open-forge-interview',
+  webDir: 'www'
+};
+
+export default config;
